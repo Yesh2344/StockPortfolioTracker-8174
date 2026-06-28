191dd0d270de21c9b3a0eb17b3047c684bae7e89
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-06-28 08:01:22 +0000

    more sensible defaults

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
         self.alpha_vantage_api_key = os.environ.get('ALPHA_VANTAGE_API_KEY')
         self.portfolio_file = os.environ.get('PORTFOLIO_FILE')
 
+# leaving a note for later
     def get_alpha_vantage_api_key(self):
         return self.alpha_vantage_api_key
 
